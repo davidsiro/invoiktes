3b8851021b7720c42b4becea4e566500a3f8d2f5
David Siro <david@catchblock.me>
2021-01-31 16:43:07 +0100

    fixed script engine classpath issues by explictly creating kts engine

--- src/main/kotlin/davidsiro/invoices/invoiceGenerator.kt
+++ src/main/kotlin/davidsiro/invoices/invoiceGenerator.kt
@@ -1,14 +1,38 @@
 package davidsiro.invoices
 
+import org.jetbrains.kotlin.cli.common.repl.ScriptArgsWithTypes
+import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngine
+import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngineFactory
+import org.jetbrains.kotlin.script.jsr223.KotlinStandardJsr223ScriptTemplate
 import java.io.*
+import javax.script.Bindings
+import javax.script.ScriptContext
+import javax.script.ScriptEngine
 import javax.script.ScriptEngineManager
+import kotlin.script.experimental.jvm.util.scriptCompilationClasspathFromContextOrStdlib
 
 fun generateInvoice(input: InputStream, output: OutputStream) {
-    val engine = ScriptEngineManager().getEngineByExtension("kts")!!
+    val engine = getScriptEngine()
 
     val inputInvoiceDef: Invoice = engine.eval(InputStreamReader(input)) as Invoice
 
     OutputStreamWriter(output).use { out ->
         generateHTMLInvoice(out, inputInvoiceDef)
     }
+}
+
+private fun getScriptEngine(): ScriptEngine {
+    return KotlinJsr223JvmLocalScriptEngine(
+        KotlinJsr223JvmLocalScriptEngineFactory(),
+        scriptCompilationClasspathFromContextOrStdlib(
+            keyNames = arrayOf(
+                "kotlin-script-util.jar",
+                "kotlin-compiler-embeddable.jar"),
+            wholeClasspath = true),
+        KotlinStandardJsr223ScriptTemplate::class.qualifiedName!!,
+        { ctx, types ->
+            ScriptArgsWithTypes(arrayOf(ctx.getBindings(ScriptContext.ENGINE_SCOPE)), types ?: emptyArray())
+        },
+        arrayOf(Bindings::class)
+    )
 }
